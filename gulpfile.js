2a9da32cc1ed5f57686a5c2c37750375356e34d9
Allan Kimmer Jensen <allankimmerjensen@gmail.com>
2014-10-15 14:58:43 +0200

    Added a gulp jshint task

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,16 @@
 'use strict';
 
 var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
 
-gulp.task('default', function() {
+gulp.task('jshint', function () {
+  return gulp.src('source/scripts/**/*.js')
+    .pipe($.jshint())
+    .pipe($.jshint.reporter('jshint-stylish'))
+    .pipe($.jshint.reporter('fail'));
+});
+
+gulp.task('default', ['jshint'], function() {
 	console.log('Awesome? Not yet but soon...');
 
 	// Just copy files for now
